fa5aab7738b6fde07544fff61036d2f8fb23ff8c
nicolas-10 <67733418+nicolas-10@users.noreply.github.com>
2021-12-25 11:02:35 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Food</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodless</li>
+    </ul>
+  </div>,
+  rootElement
+);
